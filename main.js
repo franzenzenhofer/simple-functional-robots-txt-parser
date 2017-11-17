b0546b6db89911080221de59715e2e7030c83348
Franz Enzenhofer <f.enzenhofer@gmail.com>
2017-11-17 16:25:17 +0100

    =>

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function robotsParse(txt,url,ua="Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)") =>
+function robotsParse(txt,url,ua="Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)")
 {
 	let lines = txt.split("\n");
 	let last_valid_key = "";
