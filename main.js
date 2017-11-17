f3f0d8c72848e669086a3cf41347d1f3e6eb49d2
Franz Enzenhofer <f.enzenhofer@gmail.com>
2017-11-17 15:58:38 +0100

    nr 3

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-export const parse = (txt,url,ua="Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)") =>
+const parse = (txt,url,ua="Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)") =>
 {
 	let lines = txt.split("\n");
 	let last_valid_key = "";
@@ -207,4 +207,4 @@ export const parse = (txt,url,ua="Mozilla/5.0 (compatible; Googlebot/2.1; +http:
 	return r;
 }
 
-module.export = parse;
+module.exports = parse;
